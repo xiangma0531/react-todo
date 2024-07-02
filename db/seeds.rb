3c105de167b7da86a936d23313679a4debddf5b6
xiangma0531 <shoma0531@outlook.com>
2024-07-02 22:47:49 +0900

    テストデータ生成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+SAMPLE_TODOS = [
+  {
+    title: 'Reactの勉強をする',
+  },
+  {
+    title: 'Railsの勉強をする',
+  },
+  {
+    title: 'AWSの勉強をする',
+  },
+]
+
+SAMPLE_TODOS.each do |todo|
+  Todo.create(todo)
+end
