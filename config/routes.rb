f81678e095021acf03b635142a3b54b875418c7c
xiangma0531 <shoma0531@outlook.com>
2024-07-02 23:18:26 +0900

    ルーティング修正・ディレクトリ構成修正

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       delete '/todos/destroy_all', to: 'todos#destroy_all'
-      resources :todos, only: [:index, :show, :create, :update, :destroy]
+      resources :todos, only: %i[index show create update destroy]
     end
   end
 end
